bb7a23105ab34787b55c5d5b0f55adc0eba3ac0d
CadenParry <cadenparry02@gmail.com>
2023-03-24 00:36:14 -0400

    format and remove yellow stuff

--- test/heap/ListMinHeapTest.java
+++ test/heap/ListMinHeapTest.java
@@ -1,13 +1,5 @@
 package heap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 /**
